ce67f854da234004eb05a90f8955099c9a2b134d
jayashri-dev <jayashrielumalai1602@gmail.com>
2025-02-16 15:12:44 +0530

    ErrorDetails.java

--- ErrorDetails.java
+++ ErrorDetails.java
@@ -1,28 +1,28 @@
 package com.rest.webservices.restful_web_services.exception;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class ErrorDetails {
 	
-	private LocalDate timestamp;
+	private LocalDateTime timestamp;
 	private String message;
 	private String details;
-	public ErrorDetails(LocalDate timestamp, String message, String details) {
+	public ErrorDetails(LocalDateTime timestamp, String message, String details) {
 		super();
 		this.timestamp = timestamp;
 		this.message = message;
 		this.details = details;
 	}
-	public void setTimestamp(LocalDate timestamp) {
-		this.timestamp = timestamp;
+	public LocalDateTime getTimestamp() {
+		return timestamp;
 	}
-	public void setMessage(String message) {
-		this.message = message;
+	public String getMessage() {
+		return message;
 	}
-	public void setDetails(String details) {
-		this.details = details;  
+	public String getDetails() {
+		return details;
 	}
-	
 	
 
 }
